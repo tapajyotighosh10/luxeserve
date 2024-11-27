3bec864ab45467ab294af228e9bae13197ad4a9d
tapajyotighosh10 <tapajyotighosh10@gmail.com>
2024-11-27 16:44:41 +0530

    implemented api gateway

---
+++ api-gateway/src/test/java/com/luxeserve/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxeserve.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
