e451496f4ad151b99a16a8ae8f5267b723ee9ea0
tapajyotighosh10 <tapajyotighosh10@gmail.com>
2024-11-28 16:32:21 +0530

    implemeted circuite braker, retry and rate limiter

--- user-service/src/main/java/com/luxeserve/user_service/controller/UserController.java
+++ user-service/src/main/java/com/luxeserve/user_service/controller/UserController.java
@@ -2,6 +2,11 @@ package com.luxeserve.user_service.controller;
 
 import com.luxeserve.user_service.entity.Users;
 import com.luxeserve.user_service.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService userService;
     // create
@@ -21,13 +26,30 @@ public class UserController {
        Users users1= userService.saveUser(users);
        return ResponseEntity.status(HttpStatus.CREATED).body(users1);
     }
+    int retryCount=1;
 
     // single user get
     @GetMapping("/getuser/{userId}")
+//    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
+//    @Retry(name="ratingHotelService", fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name="userRateLimiter",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<Users> getSingleUser(@PathVariable String userId){
+        logger.info("Get Single User Handler: UserController");
+        logger.info("Retry count: {}",retryCount);
+        retryCount++;
         Users users1=  userService.getUserById(userId);
         return ResponseEntity.ok(users1);
     }
+        // creating fallback method for circuitbreaker
+   public ResponseEntity<Users> ratingHotelFallback(String userId,Exception ex){
+
+   Users user =    Users.builder().email("info@gmail.com")
+               .name("Tapajyoti")
+               .about("This is dummy user because some services down")
+               .UserId("14568")
+               .build();
+        return new ResponseEntity<>(user, HttpStatus.TOO_MANY_REQUESTS);
+   }
 
     // all user get
     @GetMapping()
